cdf7511a3b5ab9f60b2f45924a590d2d818f37ef
vladimirkostadinov <60099467+vladimirkostadinov@users.noreply.github.com>
2022-09-02 14:08:52 +0300

    Delete haproxy_config directory

--- haproxy_config/update-config.sh
+++
@@ -1,1 +0,0 @@
-/usr/sbin/haproxy -f /home/${NODE_NAME}admin/jenkins/workspace/Clone-GitHub-project/haproxy_config/haproxy.cfg -c && /usr/sbin/haproxy -f /home/${NODE_NAME}admin/jenkins/workspace/Clone-GitHub-project/haproxy_config/haproxy.cfg -p /var/run/haproxy.pid -sf $(cat /var/run/haproxy.pid)
